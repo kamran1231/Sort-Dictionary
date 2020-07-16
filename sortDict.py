6ab0a9ce3bddb7d680fa2190087ed770b9d62bfe
kamran khan <48378125+kamran1231@users.noreply.github.com>
2020-07-16 20:13:46 +0530

    Add files via upload

---
+++ sortDict.py
@@ -0,0 +1,12 @@
+
+import operator
+
+d = {1:4,2:6,8:12,4:3,0:0,6:15}
+print('Original dictionary',d)
+
+sorted_d = dict(sorted(d.items(),key= operator.itemgetter(1)))
+print('Dictionary in ascending order by values:',sorted_d)
+
+
+sorted_d = dict(sorted(d.items(), key= operator.itemgetter(1),reverse=True))
+print('Dictionary in descending order by values:',sorted_d)
